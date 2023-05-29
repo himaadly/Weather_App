6353826e7214c5df3aed174d473810f1454c5a7d
Ibrahim Mohamed <hima.299068@gmail.com>
2023-05-29 19:10:36 +0300

    first push with basic folder structure

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import HomeLayout from '@layouts/HomeLayout.vue';
+export default {
+  components: {
+    HomeLayout
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <HomeLayout></HomeLayout>
+</template>
+
+<style></style>
---
+++ src/layouts/HomeLayout.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>Home layout</h1>
+</template>
+
+<style></style>
